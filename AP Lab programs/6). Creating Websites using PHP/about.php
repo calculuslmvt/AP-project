7dc1357e8990f363440551b14c7daa8ffcd7e82a
AYUSH SINGH <55044051+calculuslmvt@users.noreply.github.com>
2020-06-24 03:33:15 +0530

    ap lab file pdf

--- AP Lab programs/6). Creating Websites using PHP/about.php
+++
@@ -1,3 +0,0 @@
-<?php echo file_get_contents("header.html");?>
-<?php echo file_get_contents("navigation.html");?>
-<?php echo file_get_contents("ft.html");?>
--- AP Lab programs/6). Creating Websites using PHP/index.php
+++
@@ -1,4 +0,0 @@
-<?php echo file_get_contents("header.html");?>
-<?php echo file_get_contents("navigation.html");?>
-<?php echo file_get_contents("body.html");?>
-<?php echo file_get_contents("footer.html");?>
